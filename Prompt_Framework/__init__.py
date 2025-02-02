d086bbd92a529ba046d72a53c5417a96fa3c978b
Subhagato Adak <subhagatoadak.india@gmail.com>
2025-02-02 13:37:42 +0530

    Update __init__.py

--- Prompt_Framework/__init__.py
+++ Prompt_Framework/__init__.py
@@ -4,30 +4,30 @@ from .Prompt_Framework import Prompt_Framework
 
 # You can also import specific frameworks directly for convenience if needed
 from .Prompt_Framework import (
-    RACE,
-    CARE,
-    APE,
-    CREATE,
-    TAG,
-    CREO,
-    RISE,
-    PAIN,
-    COAST,
-    ROSES,
-    REACT
+    race_framework,
+    care_framework,
+    ape_framework,
+    create_framework,
+    tag_framework,
+    creo_framework,
+    rise_framework,
+    pain_framework,
+    coast_framework,
+    roses_framework,
+    react_framework
 )
 
 __all__ = [
-    "PromptFramework",
-    "RACE",
-    "CARE",
-    "APE",
-    "CREATE",
-    "TAG",
-    "CREO",
-    "RISE",
-    "PAIN",
-    "COAST",
-    "ROSES",
-    "REACT"
+    "Prompt_Framework",
+    "race_framework",
+    "care_framework",
+    "ape_framework",
+    "create_framework",
+    "tag_framework",
+    "creo_framework",
+    "rise_framework",
+    "pain_framework",
+    "coast_framework",
+    "roses_framework",
+    "react_framework"
 ]
