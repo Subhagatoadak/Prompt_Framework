17ab66b7de31f84a120c5f05edab7181d7137e6f
Subhagato Adak <subhagatoadak.india@gmail.com>
2025-04-21 10:41:20 +0530

    updated main_publish.yml

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Prompt_Framework",  # The package name with the capitalized letters
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),  # Automatically finds all sub-packages
     # Other metadata like author, description, etc.
 )
