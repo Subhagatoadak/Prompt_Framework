ff981596f9ee875d321d734e4a8a3a84dda77ac3
Subhagato Adak <subhagatoadak.india@gmail.com>
2025-04-21 11:13:50 +0530

    bump down

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
-# setup.py
 from setuptools import setup, find_packages
+from pathlib import Path
+
+here = Path(__file__).parent
+
+# Read the contents of your README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="prompt-framework",  # The package name with the capitalized letters
-    version="0.1.1.3",
-    packages=find_packages(),  # Automatically finds all sub-packages
-    # Other metadata like author, description, etc.
+    name="Prompt_Framework",
+    version="0.1",
+    packages=find_packages(),
+    author="Subhagato Adak",
+    author_email="subhagato.adak@gmail.com",
+    description="Prompt_Framework is a Python package that provides a set of flexible frameworks for prompt engineering. It allows seamless interchangability between various frameworks",
+    long_description=long_description,                        # ← your README
+    long_description_content_type="text/markdown",            # ← or "text/x-rst"
+    url="https://github.com/Subhagatoadak/Prompt_Framework",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+      # your runtime deps
+    ],
 )
