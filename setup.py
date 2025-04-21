7fab33aefb758d88e36150d3469abdb334e53d2d
Subhagato Adak <subhagatoadak.india@gmail.com>
2025-04-21 10:55:35 +0530

    bump up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prompt-framework",  # The package name with the capitalized letters
-    version="0.1.1.1",
+    version="0.1.1.2",
     packages=find_packages(),  # Automatically finds all sub-packages
     # Other metadata like author, description, etc.
 )
